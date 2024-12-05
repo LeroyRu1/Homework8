bea1ba7028468d4e085a735a2b4413b8f3e506a6
Leroy <LeraRu@tut.by>
2024-12-05 23:06:33 +0300

    Arrays is done

--- src/Main.java
+++ src/Main.java
@@ -62,7 +62,7 @@ public class Main {
 
         //Задача 4
         System.out.println();
-        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         for (int i = 0; i < arr.length; i++) {
             if (arr [i] % 2 != 0) {
             arr [i]++;
